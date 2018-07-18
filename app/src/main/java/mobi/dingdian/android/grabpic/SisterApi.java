78666b21627df1a91f03d00be281837761d11801
lenovok20 <lenovok20@dingdian.mobi>
2018-07-18 16:41:27 +0800

    add json source

--- app/src/main/java/mobi/dingdian/android/grabpic/SisterApi.java
+++ app/src/main/java/mobi/dingdian/android/grabpic/SisterApi.java
@@ -2,6 +2,10 @@ package mobi.dingdian.android.grabpic;
 
 import android.util.Log;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -35,13 +39,35 @@ public class SisterApi {
         return sisters;
     }
 
-    private ArrayList<Sister> parseSister(String result) {
+    private ArrayList<Sister> parseSister(String result) throws Exception {
         ArrayList<Sister> sisters = new ArrayList<>();
+        JSONObject object = new JSONObject(result);
+        JSONArray array = object.getJSONArray("results");
+        for (int i = 0; i < array.length(); i++) {
+            JSONObject results = (JSONObject) array.get(i);
+            Sister sister = new Sister();
+            sister.set_id(results.getString("_id"));
+            sister.setCreateAt(results.getString("createdAt"));
+            sister.setDesc(results.getString("desc"));
+            sister.setPublishedAt(results.getString("publishedAt"));
+            sister.setSource(results.getString("source"));
+            sister.setType(results.getString("type"));
+            sister.setUrl(results.getString("url"));
+            sister.setUsed(results.getBoolean("used"));
+            sister.setWho(results.getString("who"));
+            sisters.add(sister);
+        }
         return sisters;
     }
 
-    private byte[] readFromStream(InputStream in) {
+    private byte[] readFromStream(InputStream in) throws Exception {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         byte[] buffer = new byte[1024];
-        return buffer;
+        int len;
+        while ((len = in.read(buffer)) != -1) {
+            outputStream.write(buffer, 0, len);
+        }
+        in.close();
+        return outputStream.toByteArray();
     }
 }
